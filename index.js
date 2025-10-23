0091b1c487d1c5087af547456debf9287be1f94c
phuongwj <phamjulia9@gmail.com>
2025-10-22 22:05:30 -0300

    added base, readme and endpoints todo

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express.js, and Postgres API' })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
